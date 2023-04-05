6cda6d9550f64a64bbd158b6b1e5dfac65e9552f
lhyeonj120 <isk04387@gmail.com>
2023-04-05 17:45:32 +0900

    update main page

---
+++ src/pages/index.vue
@@ -0,0 +1,262 @@
+<template>
+  <title>Capiclean</title>
+  <!--header section start -->
+    <div class="header_section">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="logo"><router-link :to="{name: 'Home'}"><img src="@/assets/images/logo.png"></router-link></div>
+          </div>
+          <div class="col-md-9">
+            <div class="menu_text">
+              <ul>
+                <div class="togle_3">
+                  <div class="menu_main">
+                    <div class="padding_left0"><a href="#">Register</a>
+                    <span class="padding_left0"><a href="#">Login</a></span></div>
+                  </div>
+                  <div class="shoping_bag"><img src="@/assets/images/search-icon.png"></div>
+                </div>
+                <div id="myNav" class="overlay">
+                  <a href="javascript:void(0)" class="closebtn" @click="closeNav">&times;</a>
+                  <div class="overlay-content">
+                    <router-link :to="{name: 'Home'}" class="menuBtn" @click="closeNav">Home</router-link>
+                    <!-- <router-link :to="{name: 'Services'}" class="menuBtn">Services</router-link>
+                    <router-link :to="{name: 'About'}" class="menuBtn">About</router-link>
+                    <router-link :to="{name: 'Choose'}" class="menuBtn">Choose</router-link>
+                    <router-link :to="{name: 'Team'}" class="menuBtn">Team</router-link>
+                    <router-link :to="{name: 'Contact'}" class="menuBtn">Contact Us</router-link> -->
+                  </div>
+                </div>
+                <span class="navbar-toggler-icon"></span>
+                <span @click="openNav"><img src="@/assets/images/toggle-icon.png" class="toggle_menu"></span>
+              </ul>
+            </div>      
+          </div>
+        </div>
+      </div>
+    <!-- banner section start -->
+    <div class="banner_section layout_padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-5">
+            <h1 class="banner_taital">Best YOUR</h1> 
+            <h1 class="banner_taital_1">house CLEAN</h1>
+            <p class="banner_text">Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+            <div class="contact_bt"><a href="contact.html">CONTACT US<span class="contact_padding"><img src="@/assets/images/contact-icon.png"></span></a></div>
+          </div>
+          <div class="col-sm-2">
+            <div class="play_icon"><a href="#"><img src="@/assets/images/play-icon.png"></a></div>
+          </div>
+          <div class="col-sm-5">
+            <div><img src="@/assets/images/img-1.png" class="image_1"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- banner section end -->
+    </div>
+    <!-- header section end -->
+    <!-- services section start -->
+    <div class="services_section layout_padding">
+      <div class="container">
+        <h1 class="services_taital"><span>Our</span> <img src="@/assets/images/icon-1.png"> <span style="color: #1f1f1f">Services</span></h1>
+        <p class="services_text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        <div id="main_slider" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="services_section_2 layout_padding">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="box_section active">
+                      <div class="tiles_img"></div>
+                      <h3 class="tile_text active">Tile & Grout</h3>
+                      <p class="lorem_text active">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor </p>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="box_section">
+                      <div class="tiles_img_1"></div>
+                      <h3 class="tile_text">Carpet Cleaning</h3>
+                      <p class="lorem_text">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="services_section_2 layout_padding">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="box_section active">
+                      <div class="tiles_img"></div>
+                      <h3 class="tile_text active">Tile & Grout</h3>
+                      <p class="lorem_text active">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor </p>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="box_section">
+                      <div class="tiles_img_1"></div>
+                      <h3 class="tile_text">Carpet Cleaning</h3>
+                      <p class="lorem_text">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="services_section_2 layout_padding">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="box_section active">
+                      <div class="tiles_img"></div>
+                      <h3 class="tile_text active">Tile & Grout</h3>
+                      <p class="lorem_text active">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor </p>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="box_section">
+                      <div class="tiles_img_1"></div>
+                      <h3 class="tile_text">Carpet Cleaning</h3>
+                      <p class="lorem_text">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+            <i class="fa fa-angle-left"></i>
+          </a>
+          <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+            <i class="fa fa-angle-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- services section start -->
+    <!-- about section start -->
+    <div class="about_section layout_padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div><img src="@/assets/images/img-2.png" class="image_2"></div>
+          </div>
+          <div class="col-md-6">
+            <h1 class="services_taital"><span>About </span> <img src="@/assets/images/icon-1.png"> <span style="color: #1f1f1f">Cleaning</span></h1>
+            <p class="ipsum_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            <div class="contact_bt_1"><a href="#">READ MORE<span class="contact_padding"><img src="@/assets/images/contact-icon1.png"></span></a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- about section end -->
+    
+    <!-- newsletter section start -->
+    <div class="newsletter_section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="newsletter_text">Newsletter</h1>
+            <p class="tempor_text">Tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+          <div class="col-md-6">
+            <div class="mail_bt_main">
+              <input type="text" class="mail_text" placeholder="ENTER YOUR MAIL" name="Enter Tour Mail">
+              <div class="subscribe_bt"><a href="#">SUBSCRIBE</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- newsletter section end -->
+    <!-- footer section start -->
+    <div class="footer_section layout_padding">
+      <div class="container">
+        <div class="footer_main">
+          <div class="footer_left">
+            <h1 class="contact_taital"><span>Contact </span> <img src="@/assets/images/icon-2.png"> <span>Us</span></h1>
+          </div>
+          <div class="footer_left">
+            <div class="location_text"><a href="#"><img src="@/assets/images/map-icon.png"><span class="padding_left_15">Locations</span></a></div>
+          </div>
+          <div class="footer_left">
+            <div class="location_text"><a href="#"><img src="@/assets/images/call-icon.png"><span class="padding_left_15">+71 9876543210</span></a></div>
+          </div>
+          <div class="footer_left">
+            <div class="location_text"><a href="#"><img src="@/assets/images/map-icon.png"><span class="padding_left_15">demo@gmail.com</span></a></div>
+          </div>
+        </div>
+        <div class="contact_section">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="mail_section">
+                <input type="" class="email_text" placeholder="Name" name="Name">
+                <input type="" class="email_text" placeholder="Email" name="Email">
+                <input type="" class="email_text" placeholder="Phone Number" name="Phone Number">
+                <textarea class="massage_text" placeholder="Message" rows="5" id="comment" name="Message"></textarea>
+                <div class="send_bt"><a href="#">send</a></div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="map_main">
+                <div class="map-responsive">
+                  <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="280" frameborder="0" style="border:0; width: 100%;" allowfullscreen></iframe>
+                </div>
+              </div>
+              <div class="social_icon">
+                <ul>
+                  <li><a href="#"><img src="@/assets/images/fb-icon1.png"></a></li>
+                  <li><a href="#"><img src="@/assets/images/twitter-icon1.png"></a></li>
+                  <li><a href="#"><img src="@/assets/images/linkden-icon1.png"></a></li>
+                  <li><a href="#"><img src="@/assets/images/instagram-icon1.png"></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- footer section end -->
+    <!-- copyright section start -->
+    <div class="copyright_section">
+      <div class="container">
+        <p class="copyright_text">Copyright 2020 All Right Reserved By <a href="https://html.design">Free html  Templates</a></p>
+      </div>
+    </div>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+export default {
+  setup(){
+    let overlay = null;
+
+    const openNav = () => {
+      overlay = document.getElementsByClassName('overlay')[0];
+      overlay.style.width = "100%";
+    }
+    const closeNav = () => {
+      overlay.style.width = "0%";
+    }
+
+    return{
+      openNav,
+      closeNav,
+    }
+  }
+
+}
+</script>
+
+<style>
+@import "@/assets/css/bootstrap.min.css";
+@import "@/assets/css/style.css";
+@import "@/assets/css/responsive.css";
+@import "@/assets/css/jquery.mCustomScrollbar.min.css";
+@import "@/assets/css/owl.carousel.min.css";
+@import url("https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css");
+
+</style>
